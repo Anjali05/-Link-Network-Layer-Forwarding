9683979743c8fab12dee3675e836d1e1080f5b04
Anjali05 <shally.hp5@gmail.com>
2018-10-10 16:31:03 -0500

    adding code for forwarding and broadcasting(TODO timeout)

--- src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -8,8 +8,29 @@ import edu.wisc.cs.sdn.vnet.Iface;
 /**
  * @author Aaron Gember-Jacobson
  */
+
+
+/*public class switchTable{
+	//string hostname;
+	Object  macAddress;
+	Iface inIface;
+
+	public switchTable(Object macAddress, Iface inIface){
+		this.macAddress = macAddress;
+		this.inIface = inIface;
+	}
+}
+*/
 public class Switch extends Device
-{	
+{
+	//public static timeout = 15;
+
+	//switchTable table = new switchTable()
+	Map<Object, Iface> macToPort = new HashMap<Object, Iface>();
+
+   // assuming table size to be no more than 1024
+  // private static final int maxTableSize = 1024;
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -28,10 +49,50 @@ public class Switch extends Device
 	{
 		System.out.println("*** -> Received packet: " +
                 etherPacket.toString().replace("\n", "\n\t"));
-		
+		Object macAddressSource = etherPacket.getSourceMAC();
+		Object macAddressDestination = etherPacket.getDestinationMAC();
+
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
 		
 		/********************************************************************/
+
+
+		//check for destination MAC address
+		if(macToPort.containsKey(macAddressDestination)){
+			//forward
+			Iface iface = macToPort.get(macAddressDestination);
+			System.out.println("Entry found for destination address\n Forwarding packet on "+iface);
+			try{
+				sendPacket(etherPacket, iface);
+			}
+			catch (Exception){
+				System.out.println("Error in forwarding");
+			}
+
+		}
+
+		//broadcast to all interfces
+		else{
+			//Map<String,Iface> interfaces = getInterfaces();
+			for (Map.Entry<String, Iface> entry : getInterfaces().entrySet()){
+				System.out.println("Broadcasting to :\t" + etherPacket.getDestinationMAC()+ " : " + entry.getValue());
+				try{
+					sendPacket(etherPacket, entry.getValue());
+				}
+				catch (Exception){
+					System.out.println("Error in broadcasting");
+				}
+			}
+			//macToPort.put(macAddress, inIface);
+		}
+
+		//check for source MAC address
+		if(!macToPort.containsKey(macAddressSource)){
+			macToPort(macAddressSource, inIface);
+		}
 	}
+
+	//TODO timeout
+
 }
